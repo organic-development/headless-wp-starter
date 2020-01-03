93a2162b76a94618af138621fba43db8839e2ffe
Joe Webber <joe.w@theorganicagency.com>
2020-01-03 16:20:47 +0000

    Revert "Change Gatsby starter to https://github.com/GatsbyCentral/gatsby-starter-wordpress"
    This reverts commit 7fe43a95078d52ed523bfed29fbb8da9d6e02dc4.

--- frontend-gatsby/src/components/layout.js
+++ frontend-gatsby/src/components/layout.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
 
-import Navbar from './Navbar'
-import './all.sass'
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Gatsby + WordPress" />
-    <Navbar />
-    <div>{children}</div>
-  </div>
-)
+import Header from "./header"
+import "./layout.css"
 
-export default TemplateWrapper
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
